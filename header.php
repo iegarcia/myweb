7de384d8a7766e8635756247e997a603d4e87b24
iegarcia <nachogarcia003@gmail.com>
2020-06-10 19:26:54 -0300

    Preparando incorporacion de captcha y pantalla post contacto

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description"
+    content="Diseño y Desarrollo de sitios web adaptado a sus necesidades, adaptable a celular. Páginas personales, blogs, pymes, e-commerce, ecommerce, etc." />
+  <meta name="keywords"
+    content="web design, web developer, desarrollador web, diseño, web, full stack developer, html, css, javascript, js, react, php, programacion, programación, laravel, git, github" />
+
+  <title>IJEG Design | Diseño y Desarrollo Web</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+    integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+    integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous" />
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="node_modules/sweetalert2/dist/sweetalert2.min.css" />
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand navbar-logo" data-scroll-nav="0" href="">
+        <img src="" alt="" class="logo" />
+      </a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="fas fa-bars"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#inicio" data-scroll-nav="0">Inicio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#acerca" data-scroll-nav="1">Acerca</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#servicios" data-scroll-nav="2">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#trabajos" data-scroll-nav="3">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contacto" data-scroll-nav="4">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
--- index.php
+++ index.php
@@ -1,61 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+require_once("partials.php");
+?>
 
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description"
-    content="Diseño y Desarrollo de sitios web adaptado a sus necesidades, adaptable a celular. Páginas personales, blogs, pymes, e-commerce, ecommerce, etc." />
-  <meta name="keywords"
-    content="web design, web developer, desarrollador web, diseño, web, full stack developer, html, css, javascript, js, react, php, programacion, programación, laravel, git, github" />
-
-  <title>IJEG Design | Diseño y Desarrollo Web</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-    integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
-    integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous" />
-  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-  <link rel="stylesheet" href="node_modules/sweetalert2/dist/sweetalert2.min.css" />
-  <link rel="stylesheet" href="css/style.css" />
-</head>
-
-<body>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg">
-    <div class="container">
-      <a class="navbar-brand navbar-logo" data-scroll-nav="0" href="">
-        <img src="" alt="" class="logo" />
-      </a>
-      <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="fas fa-bars"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#inicio" data-scroll-nav="0">Inicio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#acerca" data-scroll-nav="1">Acerca</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#servicios" data-scroll-nav="2">Servicios</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#trabajos" data-scroll-nav="3">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#contacto" data-scroll-nav="4">Contacto</a>
-          </li>
-          <!-- <li class="nav-item">
-            <a class="nav-link disabled" href="#blog" data-scroll-nav="5">Blog (proximamente)</a>
-          </li> -->
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- End Navbar -->
 
   <!-- Banner Image -->
   <div class="banner text-center" data-scroll-index="0" id="inicio">
@@ -120,7 +66,6 @@
       </div>
     </div>
   </div>
-
   <!-- End About -->
 
   <!-- Services -->
@@ -406,21 +351,3 @@
       </div>
     </div>
   </div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-    integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
-    crossorigin="anonymous"></script>
-  <script src="node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
-  <!-- magnific-popup -->
-  <script src="js/jquery.fancybox.min.js"></script>
-
-  <!-- scrollIt js -->
-  <script src="js/scrollIt.min.js"></script>
-
-  <!-- isotope.pkgd.min js -->
-  <script src="js/isotope.pkgd.min.js"></script>
-  <script src="js/style.js"></script>
-</body>
-
-</html>
