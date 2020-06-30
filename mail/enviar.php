7442cf71755d74a9c86372408224eab4aa198bac
iegarcia <nachogarcia003@gmail.com>
2020-06-30 18:30:51 -0300

    success message

--- mail/enviar.php
+++ mail/enviar.php
@@ -68,7 +68,7 @@ try {
 		</div>
 	</div>
 
-	<script src="node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+	<script src="../node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
 </body>
 
 </html>
